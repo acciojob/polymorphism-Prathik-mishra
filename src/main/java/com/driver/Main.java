0bf26d81bb3d774011351ed4816f3461968d43e5
Prathik Mishra <126965610+Prathik-mishra@users.noreply.github.com>
2024-03-11 17:03:06 +0530

    fixed code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,21 +1,25 @@
 package com.driver;
 
 public class Main {
-  class Product{
+  public static class Product{
     public int product(int x, int y) {
-      return x+y;
+      return x*y;
     }
     public int product(int x, int y, int z) {
-      return x+y+z;
+      return x*y*z;
     }
     public double product(double x, double y) {
-      return x+y;
+      return x*y;
     }
   }
       public static void main(String[] args){
+
         Product p = new Product();
-        p.product(1,2);
-        p.product(1,2,3);
-        p.product(1.0,2.0);
+
+        int ans =  p.product(10,20);
+        int ans1 = p.product(10,20,30);
+        double ans2 = p.product(10.0,20.0);
+
+        System.out.println(ans+" "+ans1+" "+ans2);
       }
 }
